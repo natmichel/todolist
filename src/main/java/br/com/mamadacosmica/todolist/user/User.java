3ee55c680c0e7be1ab58053db072dd34843c71f7
natmichel <nmichel582@gmail.com>
2023-10-09 17:15:01 -0300

    primeiro commit

---
+++ src/main/java/br/com/mamadacosmica/todolist/user/User.java
@@ -0,0 +1,27 @@
+package br.com.mamadacosmica.todolist.user;
+
+public class User {
+    private String nome;
+    private String username;
+    private String senha;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
